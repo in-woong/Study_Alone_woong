4343ad48081dd0532f15afdab6b0fa8071595911
woong <inwoong100@gmail.com>
2022-09-06 21:46:58 +0900

    기본 redux store 생성

---
+++ learn-redux-middleware/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
