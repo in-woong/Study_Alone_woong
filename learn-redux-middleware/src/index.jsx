e72e29265ded68abe5d8a3be4e6e8c8b033df0a4
woong <inwoong100@gmail.com>
2022-09-06 23:32:51 +0900

    redux middleware redux-thunk

--- learn-redux-middleware/src/index.jsx
+++ learn-redux-middleware/src/index.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import App from './App';
 import loggerMiddleware from './lib/loggerMiddleware';
 import rootReducer from './modules';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
