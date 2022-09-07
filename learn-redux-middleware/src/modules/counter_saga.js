fcf72b73114fe6fe3feb7246893845a7dacb915d
woong <inwoong100@gmail.com>
2022-09-07 23:19:12 +0900

    redux middleware redux-saga

--- learn-redux-middleware/src/modules/counter_saga.js
+++ learn-redux-middleware/src/modules/counter_saga.js
@@ -1,5 +1,12 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import {
+  delay,
+  put,
+  select,
+  takeEvery,
+  takeLatest,
+  throttle,
+} from 'redux-saga/effects';
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
@@ -11,14 +18,18 @@ export const decrease = createAction(DECREASE);
 function* increaseSaga() {
   yield delay(1000);
   yield put(increase());
+  const number = yield select((state) => state.counter);
+  console.log(`현재 값은 ${number} 입니다.`);
 }
 function* decreaseSaga() {
   yield delay(1000);
   yield put(decrease());
+  const number = yield select((state) => state.counter);
+  console.log(`현재 값은 ${number} 입니다.`);
 }
 
 export function* counterSaga() {
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
 }
 
