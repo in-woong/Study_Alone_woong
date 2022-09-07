7fcde1e3adcfa678565ebd2b1aa8ec5c2df68e8a
woong <inwoong100@gmail.com>
2022-09-07 23:49:46 +0900

    feature: css 기본 설정

--- blog/blog-frontend/src/page/PostListPage.jsx
+++ blog/blog-frontend/src/page/PostListPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 const PostListPage = () => {
-  return <div>포스트 리스트</div>;
+  return (
+    <div>
+      포스트 리스트
+      <Button>버튼</Button>
+    </div>
+  );
 };
 
 export default PostListPage;
