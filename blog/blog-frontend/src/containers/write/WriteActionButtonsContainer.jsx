8c88c1f64b34cd2aca28574a92e7580102357c9c
woong <inwoong100@gmail.com>
2022-09-12 15:12:53 +0900

    feature/frontEnd: 글 post 기능 apit 와 연동

---
+++ blog/blog-frontend/src/containers/write/WriteActionButtonsContainer.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import {
+  useDispatch,
+  useSelector,
+} from '../../../node_modules/react-redux/es/exports';
+import { useNavigate } from '../../../node_modules/react-router-dom/index';
+import WriteActionButtons from '../../components/write/WriteActionButtons';
+import { writePost } from '../../modules/write';
+
+const WriteActionButtonsContainer = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { title, body, tags, post, postError } = useSelector(({ write }) => ({
+    title: write.title,
+    body: write.body,
+    tags: write.tags,
+    post: write.post,
+    postError: write.postError,
+  }));
+
+  const onPublish = () => {
+    dispatch(
+      writePost({
+        title,
+        body,
+        tags,
+      }),
+    );
+  };
+
+  const onCancel = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    if (post) {
+      const { _id, user } = post;
+      navigate(`/@${user.username}/${_id}`);
+    }
+    if (postError) {
+      console.log(postError);
+    }
+  }, [navigate, post, postError]);
+
+  return <WriteActionButtons onPublish={onPublish} onCancel={onCancel} />;
+};
+
+export default WriteActionButtonsContainer;
--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -2,15 +2,17 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
 import loading from './loading';
+import write, { writeSaga } from './write';
 import user, { userSaga } from './user';
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
+  write,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga()]);
 }
 export default rootReducer;
