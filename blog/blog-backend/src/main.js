19447f302d8673327c138f68acee195b1a2812fd
woong <inwoong100@gmail.com>
2022-09-13 11:16:15 +0900

    feature: frontEnd build 및 서버에서 배포

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -4,9 +4,13 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+
 import api from './api';
-import createFakeData from './createFakeData';
 import jwtMiddleware from './lib/jwtMiddleware';
+import { setMaxListeners } from 'events';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -28,6 +32,14 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (setMaxListeners.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'public/index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
